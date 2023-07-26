529d99da248da01952d2b8ce4fef7a7ad3262579
Daria Skavinskaya <115110923+DariaSka@users.noreply.github.com>
2023-07-26 12:01:37 +0400

    upd

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 from fastai.vision.all import *
-import gradio as gr 
+import gradio as gr
 
 
 learn = load_learner('khachapuri_classifier.pkl')
 
-categories =  learn.dls.vocab
+categories = learn.dls.vocab
+
 
 def predict(img):
-    #img = PILImage.create(img)
-    pred,pred_idx,probs = learn.predict(img)
-    return dict(zip(categories,map(float,probs)))
+    # img = PILImage.create(img)
+    pred, pred_idx, probs = learn.predict(img)
+    return dict(zip(categories, map(float, probs)))
 
-image = gr.inputs.Image(shape=(192,192))
+
+image = gr.inputs.Image(shape=(192, 192))
 label = gr.outputs.Label()
-examples = ['achma.jpg','ajaruli.jpg','guruli.jpg','penovani.jpg']
+examples = ['achma.jpg', 'ajaruli.jpg', 'guruli.jpg', 'penovani.jpg']
 
-#intf = gr.Interface(fn=predict, inputs=image, outputs=label, examples = examples)
-intf = gr.Interface(fn=predict, 
-             inputs=image, 
-             outputs=gr.outputs.Label(num_top_classes=3),
-             examples = examples)
+# intf = gr.Interface(fn=predict, inputs=image, outputs=label, examples = examples)
+intf = gr.Interface(fn=predict,
+                    inputs=image,
+                    outputs=gr.outputs.Label(num_top_classes=3),
+                    examples=examples)
 
-intf.launch(inline = False)
+intf.launch(inline=False)
